da676f3278273660af718270a207b2bb38ade42f
Dev7Ayman <89243487+Dev7Ayman@users.noreply.github.com>
2024-01-30 15:34:14 +0300

    Update YouTube.js

--- YouTube.js
+++ YouTube.js
@@ -3,12 +3,15 @@
   "versionName": "19.02.39",
   "Msg": "New update is available, to continue in our app please update it.
 
-What's new:
-- Performance Improvements.
-- Bug Fixes.
-- New Features introduced.
-- Others minor changes.
+🔥 What's new in YouTube Plus 🎥 V2.0❓
+
+🟥 [Base] Update Updated to 19.02.39 - Play Store
+➕ [Added] All new features.
+🛠 [Fixed] Problem of video clips stopping minutes after playing the video
+🛠 [Fixed] Toast "MicroG does not run in the background"
+🛠 [Fixed] All problems
+👌 [Misc] Many other fixes and improvements that we forgot
 
 Thanks for using",
-  "downloadLink": "https://t.me/adwhats"
+  "downloadLink": "https://t.me/Android_play_apks/4582?single"
 }
